5b029ad342f996a7340929a509df086c1051f875
Likai Liu <likailiu2@gmail.com>
2022-11-24 12:53:09 +0000

    Fix lint errors

--- fetcher.js
+++ fetcher.js
@@ -6,14 +6,14 @@ const url = process.argv[2];
 const path = process.argv[3];
 
 request(url, (error, response, body) => {
-  if(error) {
+  if (error) {
     if (error.errno === "ENOTFOUND") {
-      console.log("Invalid URL, please try again.")
+      console.log("Invalid URL, please try again.");
     } else {
       console.log("error", error);
     }
-  };
-  
+  }
+
   if (!error) {
     fs.writeFile(path, body, err => {
       if (err) {
